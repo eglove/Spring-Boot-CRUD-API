c1d547c6a31203c640ede82464da178d688f5e97
eglove <hello@ethang.email>
2020-09-19 08:34:14 -0500

    add spring data rest

--- src/main/java/com/ethan/springboot/crud/dao/EmployeeRepository.java
+++ src/main/java/com/ethan/springboot/crud/dao/EmployeeRepository.java
@@ -3,5 +3,7 @@ package com.ethan.springboot.crud.dao;
 import com.ethan.springboot.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Default path for API is Entity name lowercase with s on end. (Employee = employees)
+/*@RepositoryRestResource(path="members")*/
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
